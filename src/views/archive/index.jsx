c3b8fec7c364fe0403a0171d247633682d6a10f0
nagger <315043140@qq.com>
2018-06-19 23:33:54 +0800

    rebuild

--- src/views/archive/index.jsx
+++ src/views/archive/index.jsx
@@ -3,7 +3,6 @@ import injectSheet from 'react-jss'
 import { Detail } from '../../styledComponents'
 
 const styles = {}
-const archive = [{}]
 class Archive extends React.Component {
   componentDidMount() {
     document.documentElement.scrollIntoView()
